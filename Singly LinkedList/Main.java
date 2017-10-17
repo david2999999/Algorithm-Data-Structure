4f1a8eb278e0cab4636ddd16af27141da0a335d8
David Jiang <djiang86@binghamton.edu>
2017-10-17 18:35:39 +0000

    Singly LinkedList Main

---
+++ Singly LinkedList/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+ 
+    public static void main(String[] args) {
+        SinglyLinkedList mylist = new SinglyLinkedList();
+        mylist.insertFirst(100);
+        mylist.insertFirst(50);
+        mylist.insertFirst(99);
+        mylist.insertFirst(88);
+        mylist.insertLast(9999999);
+         
+        mylist.displayList();
+    }
+ 
