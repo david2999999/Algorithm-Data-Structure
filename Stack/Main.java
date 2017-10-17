b8996c7ad39a82ffabe379418dbdd88270a62141
David Jiang <djiang86@binghamton.edu>
2017-10-17 16:28:28 +0000

    Added Queue.java

--- Stack/Main.java
+++ Stack/Main.java
@@ -1,7 +1,7 @@
-public class App {
+public class Main {
  
     public static void main(String[] args) {
-        Stack theStack = new Stack(3);
+        // StackLong theStack = new StackLong(3);
 //      theStack.push(20);
 //      theStack.push(40);
 //      theStack.push(60);
@@ -17,7 +17,7 @@ public class App {
      
     public static String reverseString(String str){
         int stackSize = str.length(); // get the max stack size
-        Stack theStack = new Stack(stackSize); // we make the stack
+        StackChar theStack = new StackChar(stackSize); // we make the stack
         for(int j = 0; j < str.length(); j++){
             char ch = str.charAt(j); // getting a char from the input string
             theStack.push(ch);
