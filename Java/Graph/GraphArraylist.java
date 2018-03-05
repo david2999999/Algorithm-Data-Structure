5921e63f762df372c2ed2e537b68602054cde892
David Jiang <djiang86@binghamton.edu>
2018-03-05 20:07:45 +0000

    graph with arraylist

---
+++ Java/Graph/GraphArraylist.java
@@ -0,0 +1,43 @@
+package Graph;
+
+import java.util.ArrayList;
+
+// this will be an directional graph
+public class GraphArraylist {
+    // number of vertices and edges
+    private int vCount, eCount;
+
+    // each of the elements inside adjacents will be an arrayList
+    private ArrayList[] adjacents;
+
+    public GraphArraylist(int vCount){
+        this.vCount = vCount;
+        this.eCount = 0;
+        this.adjacents = new ArrayList[vCount];
+
+        for (int i = 0; i < vCount; i++){
+            adjacents[i] = new ArrayList<Integer>();
+        }
+    }
+
+    public int getVertexCount(){
+        return vCount;
+    }
+
+    public int getEdgeCount(){
+        return eCount;
+    }
+
+    // add the edge between each vertex
+    public void addEdge(int src, int dest){
+        adjacents[src].add(dest);
+        eCount++;
+    }
+
+    // return all of the adjacent vertices
+    public Object[] adj(int src){
+        return adjacents[src].toArray();
+    }
+
+
+}
